1298d8e3cdda7c429178bcc7f78fbb843d7ffe6b
alexperronnet <alex@alexperronnet.com>
2020-07-28 21:17:41 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,36 +8,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-emotion',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-plugin-canonical-urls',
-      options: { siteUrl: 'https://alexperronnet.com' }
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://alexperronnet.com',
-        sitemap: 'https://alexperronnet.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-htaccess',
-      options: {
-        RewriteBase: true,
-        https: true,
-        www: false,
-        ErrorDocument: 'ErrorDocument 404 /index.html',
-        custom: `
-        <IfModule mod_headers.c>
-        Header set Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" env=HTTPS
-        Header set X-XSS-Protection "1; mode=block"
-        Header always append X-Frame-Options SAMEORIGIN
-        Header set X-Content-Type-Options nosniff
-        </IfModule>
-        `
-      }
-    }
+    'gatsby-plugin-offline'
   ]
 }
