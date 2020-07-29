be663ccddc55d35e1c8aa118bfb991b6042c753f
alexperronnet <alex@alexperronnet.com>
2020-07-29 18:37:45 +0200

    Update metas.js

--- src/components/metas.js
+++ src/components/metas.js
@@ -10,49 +10,20 @@ export default function Metas() {
         siteMetadata {
           title
           description
+          siteUrl
         }
       }
     }
   `)
 
-  const { title, description } = data.site.siteMetadata
+  const { title, description, siteUrl } = data.site.siteMetadata
 
   return (
-    <Helmet
-      htmlAttributes= {{
-        lang: 'en'
-      }}
-      title= { title }
-      meta= {[
-        {
-          name: 'description',
-          content: description
-        },
-        {
-          name: 'twitter:description',
-          content: description
-        },
-        {
-          name: 'twitter:title',
-          content: title
-        },
-        {
-          name: 'twitter:card',
-          content: 'summary_large_image'
-        },
-        {
-          property: 'og:description',
-          content: description
-        },
-        {
-          property: 'og:title',
-          content: title
-        },
-        {
-          property: 'og:image',
-          content: 'https://alexperronnet.com/og-image.png'
-        }
-      ]}
-    />
+    <Helmet>
+      <html lang='en' />
+      <title>{ title }</title>
+      <meta name='description' content={ description } />
+      <link rel='canonical' href={ siteUrl } />
+    </Helmet>
   )
 }
