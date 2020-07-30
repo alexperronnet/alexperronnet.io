7a6fee3b9b91bca822a9ca31d9e2d680fb40b67b
alexperronnet <alex@alexperronnet.com>
2020-07-30 14:36:01 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Alex Perronnet',
     description: 'Hello everyone, welcome to my personnal website. I am Alex Perronnet, a french freelance developer and designer. I am also an open-source contributor and a content creator.',
-    siteUrl: 'https://alexperronnet.com'
+    siteUrl: 'https://alexperronnet.io'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
