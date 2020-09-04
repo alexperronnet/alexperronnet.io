00c94bdbc4160b14530c66797319fb13440ebf84
Alex Perronnet <alexperronnet@outlook.com>
2020-09-05 00:30:34 +0200

    Update

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import Metas from "./metas"
 import Notice from "../sections/notice"
 import Footer from "../sections/footer"
+import "../styles/index.scss"
+import "typeface-roboto"
 
 const Layout = ({ children }) => {
   return (
